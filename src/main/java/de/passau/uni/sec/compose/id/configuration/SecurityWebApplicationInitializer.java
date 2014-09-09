792a6b51073fd73f6c008ec061ce5605bd07346c
nopbyte <juandabytes@gmail.com>
2014-09-09 14:07:36 +0200

    Obsolete files...

--- src/main/java/de/passau/uni/sec/compose/id/configuration/SecurityWebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package de.passau.uni.sec.compose.id.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends
-        AbstractSecurityWebApplicationInitializer {
-	
-	
-}
