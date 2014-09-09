8a3873f3a26d5c437601598f303fc019fb027642
nopbyte <juandabytes@gmail.com>
2014-09-09 14:05:03 +0200

    Creation of War-file. Code from Daniel Nachtmann

--- src/main/java/de/passau/uni/sec/compose/id/configuration/IdentityManagmentWebXml.java
+++ src/main/java/de/passau/uni/sec/compose/id/configuration/IdentityManagmentWebXml.java
@@ -3,11 +3,14 @@ package de.passau.uni.sec.compose.id.configuration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
+import de.passau.uni.sec.compose.id.start.Application;
+
 public class IdentityManagmentWebXml extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(IdentityManagmentWebXml.class);
+        //TODO changed return to application class
+        return application.sources(Application.class);
     }
 
 }
