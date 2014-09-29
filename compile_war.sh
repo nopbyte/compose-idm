edb04825e2161f632a0edeb5f4334025b2e98ffa
nopbyte <juandabytes@gmail.com>
2014-09-29 18:43:48 +0200

    removed repeated build

--- compile_war.sh
+++ compile_war.sh
@@ -5,9 +5,8 @@ sed '/JAR_OR_WAR/q' template_build.gradle>final.gradle.build
 cat war_part_build.gradle>>final.gradle.build
 awk 'f;/JAR_OR_WAR/{f=1}' template_build.gradle>>final.gradle.build
 gradle -x test build -b final.gradle.build
-gradle clean -b final.gradle.build
 gradle build -x test -b final.gradle.build
-gradle wrapper -b final.gradle.build
-./gradlew build -x test
+#gradle wrapper -b final.gradle.build
+#./gradlew build -x test
 rm final.gradle.build
 
