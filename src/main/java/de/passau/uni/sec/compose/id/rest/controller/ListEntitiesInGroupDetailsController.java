4f41e8bb18d91300a3d74a2fb85d1bd96e281f66
nopbyte <juandabytes@gmail.com>
2014-10-28 11:17:47 +0100

    Implementing functionality to get all Entities inside a group

---
+++ src/main/java/de/passau/uni/sec/compose/id/rest/controller/ListEntitiesInGroupDetailsController.java
@@ -0,0 +1,74 @@
+package de.passau.uni.sec.compose.id.rest.controller;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import de.passau.uni.sec.compose.id.common.exception.IdManagementException;
+import de.passau.uni.sec.compose.id.core.domain.IPrincipal;
+import de.passau.uni.sec.compose.id.core.event.GetGroupEvent;
+import de.passau.uni.sec.compose.id.core.event.ListEntitiesInGroupEvent;
+import de.passau.uni.sec.compose.id.core.event.ListPendingUserMembershipsEvent;
+import de.passau.uni.sec.compose.id.core.event.ListUsersInGroupEvent;
+import de.passau.uni.sec.compose.id.core.service.GroupService;
+import de.passau.uni.sec.compose.id.core.service.ListEntitiesGroupService;
+import de.passau.uni.sec.compose.id.core.service.ListUsersGroupService;
+import de.passau.uni.sec.compose.id.core.service.security.RestAuthentication;
+import de.passau.uni.sec.compose.id.rest.messages.GroupResponseMessage;
+
+@Controller
+@RequestMapping("/idm/group_entities")
+public class ListEntitiesInGroupDetailsController {
+private static Logger LOG = LoggerFactory.getLogger(ListEntitiesInGroupDetailsController.class);
+	
+	
+	@Autowired
+    private RestAuthentication authenticator;
+	
+	@Autowired
+	private ListEntitiesGroupService listEntitiesService;
+    
+	
+	
+	@RequestMapping(value="/{groupId}/", method=RequestMethod.GET)
+    public @ResponseBody ResponseEntity<Object> getMembershipsUser(   @RequestHeader("Authorization") String token,
+    		@PathVariable(value="groupId") String uid, UriComponentsBuilder builder){
+				
+			Collection<String> cred = new LinkedList<String>();
+			cred.add(token);
+		    	try{
+    				 //This method just authenticates... it doesn't do access control
+    				 Collection<IPrincipal> principals = authenticator.authenticatePrincipals(LOG,cred);
+    				 Object obj= listEntitiesService.listAllEntities(new ListEntitiesInGroupEvent(principals,uid));
+    				 return new ResponseEntity<Object>(obj, HttpStatus.OK);
+    				 
+		    	 }
+		    	 catch(IdManagementException idm){
+		    		 //since the creation of the exception generated the log entries for the stacktrace, we don't do it again here
+		    		 return new ResponseEntity<Object>(idm.getErrorAsMap(), HttpStatus.valueOf(idm.getHTTPErrorCode()));
+		    	 } 
+		    	 catch(Exception e)
+		    	 {
+		    		 String s = IdManagementException.getStackTrace(e);
+		    		 LOG.error(s);
+		    		 return new ResponseEntity<Object>(null, HttpStatus.INTERNAL_SERVER_ERROR);	 
+		    	 }
+    	 
+    	 
+        }
+
+
+    
+}
