b30a7710b8562be80aaf884bba0721ede0c7ff1b
nopbyte <juandabytes@gmail.com>
2014-11-26 12:35:52 +0100

    Wrong if...! :(

--- src/main/java/de/passau/uni/sec/compose/id/rest/controller/cors/SimpleCORSFilter.java
+++ src/main/java/de/passau/uni/sec/compose/id/rest/controller/cors/SimpleCORSFilter.java
@@ -14,7 +14,7 @@ public class SimpleCORSFilter implements Filter {
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
-		if(response.getHeader("Access-Control-Allow-Origin")!=null)
+		if(response.getHeader("Access-Control-Allow-Origin")==null)
 			response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
